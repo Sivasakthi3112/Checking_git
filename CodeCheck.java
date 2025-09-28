46040ea4e2746872c7010b5f619b31f74465aa36
sivasakthi3112 <sakthisiva31122000@gmail.com>
2025-09-28 22:58:38 +0530

    Second commit da punda

--- CodeCheck.java
+++ CodeCheck.java
@@ -1,5 +1,6 @@
-public class CodeCheck{
+public class Codepublic{
     public static void main(String args[]){
         System.out.println("am ready");
+        System.out.println("new line");
     }
 }
